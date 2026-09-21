41f613f10d345e300e50654a3c3b31720d50a4bb
Pedro <peemanuel68@gmail.com>
2026-09-21 19:01:55 -0300

    Update Sistema de Autenticação.py

--- Sistema de Autenticação.py
+++ Sistema de Autenticação.py
@@ -8,7 +8,7 @@ print('Lembre-se: o login e a senha precisam ter pelo menos 5 caracteres cada e 
 login_usuario = input('Digite um nome de login: ')
 senha_usuario = input('Digite sua senha: ')
 
-# 1. TESTA A PRIMEIRA TENTATIVA
+# 1. Testa a primeira tentativa
 if len(login_usuario) < 5 or len(senha_usuario) < 5:
     print('\nErro: O login e a senha precisam ter pelo menos 5 caracteres.')
     print('Você tem mais UMA tentativa para se cadastrar.\n')
@@ -17,7 +17,7 @@ if len(login_usuario) < 5 or len(senha_usuario) < 5:
     login_usuario = input('Digite um nome de login: ')
     senha_usuario = input('Digite sua senha: ')
 
-    # Valida tudo da segunda chance (tamanho OU igualdade)
+    # Valida tudo da segunda chance (tamanho ou igualdade)
     if len(login_usuario) < 5 or len(senha_usuario) < 5 or login_usuario == senha_usuario:
         print('\nCadastro cancelado por dados inválidos.')
         cadastro_valido = False 
@@ -30,12 +30,12 @@ elif login_usuario == senha_usuario:
     login_usuario = input('Digite um nome de login: ')
     senha_usuario = input('Digite sua senha: ')
     
-    # Valida TUDO da segunda chance (tamanho OU igualdade)
+    # Valida tudo da segunda chance (tamanho OU igualdade)
     if len(login_usuario) < 5 or len(senha_usuario) < 5 or login_usuario == senha_usuario:
         print('\nCadastro cancelado por dados inválidos.')
         cadastro_valido = False
 
-# --- ETAPA DE LOGIN (Só executa se o cadastro terminou VÁLIDO) ---
+# Etapa de login (Executa somente se o cadastro terminou válido)
 if cadastro_valido:
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -47,7 +47,7 @@ if cadastro_valido:
     if login_usuario == login_plataforma and senha_usuario == senha_plataforma:
         print('\nAcesso permitido com sucesso')
     else: 
-        # 2ª tentativa de login
+        # 2ª Tentativa de login
         print('\nAcesso negado, verifique os campos de login e senha')
         print('Você só tem mais duas tentativas \n')
         
@@ -57,7 +57,7 @@ if cadastro_valido:
         if login_usuario == login_plataforma and senha_usuario == senha_plataforma:
             print('\nAcesso permitido com sucesso')
         else: 
-            # 3ª e última tentativa de login
+            # 3ª Última tentativa de login
             print('\nAcesso negado, verifique os campos de login e senha')
             print('Você só tem mais uma tentativa\n')
             
